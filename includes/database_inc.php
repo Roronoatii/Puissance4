5bcd9d713510a4e7b0b37efa7dc77d3168685443
Roronoatii <p.rivallin@protonmail.com>
2022-11-19 17:56:34 +0100

    database

--- includes/database_inc.php
+++ includes/database_inc.php
@@ -1,5 +1,15 @@
 <?php 
+function connectDatabase(){
 $user = "root";
 $pass = "root";
 $dbh = new PDO('mysql:host=localhost;dbname=puissance4', $user, $pass);
+    try
+    {
+        $bdd = new PDO('mysql:host=localhost;dbname=puissance4;charset = utf8','root','root');
+        return $bdd;
+    }catch(Exception $e)
+    {
+        die('erreur'.$e->getMessage());
+    }
+}
 ?> 
