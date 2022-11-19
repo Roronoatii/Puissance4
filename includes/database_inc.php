ae3260eb741540ee7cea205729474cde4d555e18
Romain.jhr <romjah@orange.fr>
2022-11-19 18:00:36 +0100

    erreur corrigée pour le contact.php

--- includes/database_inc.php
+++ includes/database_inc.php
@@ -1,5 +1,5 @@
 <?php 
 $user = "root";
 $pass = "root";
-$dbh = new PDO('mysql:host=localhost;dbname=puissance4', $user, $pass);
+$dbh = new PDO('mysql:host=localhost;dbname=Puissance-4', $user, $pass);
 ?> 
