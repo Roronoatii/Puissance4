ac71a6f37f98e0e70166518405def5b1f08ad827
Oceeeee <oceanebilly23@gmail.com>
2022-11-26 16:22:10 +0100

    fix alert

--- alpha/register.php
+++ alpha/register.php
@@ -10,7 +10,7 @@
     <link href="https://www.dafontfree.net/embed/bXVsaS1zZW1pYm9sZCZkYXRhLzE2L20vNzg2NTYvTXVsaS1TZW1pQm9sZC50dGY" rel="stylesheet" type="text/css"/>
     <script src="./password-validation.js"></script>
     <script>
-        function handlePasswordChange(password) {alert('e');
+        function handlePasswordChange(password) {
             const passwordLevel = getPasswordLevel(password);
             const passwordValidationElement = document.getElementById("passwordValidation");
             passwordValidationElement.setAttribute('LEVEL', passwordLevel);
