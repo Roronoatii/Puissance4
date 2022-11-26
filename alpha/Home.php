2283881f239def050bd6e2d1f0c4b8faa07ca73e
EstebanDardillac <110052260+EstebanDardillac@users.noreply.github.com>
2022-11-26 15:56:57 +0100

    nouveau home

--- alpha/Home.php
+++ alpha/Home.php
@@ -7,13 +7,17 @@
     <title>Home</title>
     <script src="https://kit.fontawesome.com/81dc5c492f.js" crossorigin="anonymous"></script>
     <link href="Home.css" rel="stylesheet">
-   
+    <link href="view/footer.css" rel="stylesheet">
+    <link href="view/header2.css" rel="stylesheet">
+
 </head>
 <body>
     <section class="backhead">
         <header>
             <?php
+            
             include 'view/header_inc.php';
+            
             ?>
         </header>
     <section class="bottomHeader">
