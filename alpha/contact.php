3388532cf44ee292dfaf85b5c2ecc471eb72a415
EstebanDardillac <110052260+EstebanDardillac@users.noreply.github.com>
2022-11-26 15:54:10 +0100

    nouveau contact

--- alpha/contact.php
+++ alpha/contact.php
@@ -7,6 +7,8 @@
     <title>Nous Contacter</title>
     <link rel="stylesheet" href="contact.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="view/footer.css" rel="stylesheet">
+    <link href="view/header2.css" rel="stylesheet">
 </head>
 <body>
     <header>    
